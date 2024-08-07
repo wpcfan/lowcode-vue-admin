cfabc1bbacb3676b95aa3eeadff0f3bf75f080ff
Peng Wang <wpcfan@gmail.com>
2024-08-07 17:36:20 +0800

    update the pages

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-      <h2>About Page</h2>
-      <p>This is the about page.</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AboutPage',
-  }
-  </script>
-  
-  <style scoped>
-  /* 样式根据需要添加 */
-  </style>
+  <div>
+    <h2>About Page</h2>
+    <p>This is the about page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
+
+<style scoped>
+/* 样式根据需要添加 */
+</style>
   
